990298077371171d78e2b2647cfff6a04c8ed14e
jungmin <qkrwjdals302@naver.com>
2022-11-06 04:20:49 +0900

    Chore: 스타일관련 라이브러리 설치, 글로벌스타일 적용 - mui, emotion 설치

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Global } from "@emotion/react";
 import App from "./App";
+import globalStyles from "styles/globalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
 );
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,31 @@
+import { css } from "@emotion/react";
+
+const globalStyles = css`
+  * {
+    box-sizing: border-box;
+  }
+
+  html,
+  body,
+  #app {
+    height: 100%;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export default globalStyles;
