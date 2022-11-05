554d683d7b453d377ffaca18f290b7d5de9fd75c
moonkorea00 <moonkorea00@gmail.com>
2022-11-05 22:56:45 +0900

    feat : date atom

---
+++ src/store/atoms/date.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const startDate = atom({
+  key: "startDate",
+  default: new Date("2022,02,01"),
+});
+
+export const endDate = atom({
+  key: "endDate",
+  default: new Date("2022,02,07"),
+});
