2cf5ac04bd8c9b8521c3a01db03fcfd2a0b68e4c
suzz-in <o_o_su_jin_@naver.com>
2022-11-06 01:49:16 +0900

    feat: 광고관리  데이터get

--- src/components/atoms/advertis.js
+++ src/components/atoms/advertis.js
@@ -3,7 +3,7 @@ import { atom } from "recoil";
 import { Adservice } from "../service/Adservice";
 
 const adservices = new Adservice(
-  AxiosHttpClient(
+  new AxiosHttpClient(
     `https://s3.us-west-2.amazonaws.com/secure.notion-static.com/05a1a9db-897f-4e9d-87bd-1e81a03d8e04/wanted_FE_ad-list-data-set.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221105%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221105T155917Z&X-Amz-Expires=86400&X-Amz-Signature=d1fefd946d8547e1d61753cd655cade39c896eb98ade5154359c2d995f73f82f&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22wanted_FE_ad-list-data-set.json%22&x-id=GetObject`,
   ),
 );
--- src/components/pages/Advertisement.jsx
+++ src/components/pages/Advertisement.jsx
@@ -1,21 +1,21 @@
-import { getData } from "api/request";
+import { getAdvertisState } from "components/atoms/advertis";
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+// import { useEffect } from "react";
+import { useRecoilValue } from "recoil";
 
 const Advertisement = () => {
-  const baseURL = `https://s3.us-west-2.amazonaws.com/secure.notion-static.com/05a1a9db-897f-4e9d-87bd-1e81a03d8e04/wanted_FE_ad-list-data-set.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221105%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221105T133227Z&X-Amz-Expires=86400&X-Amz-Signature=9e4b8b52f77254b264a50adbee71fac6ee0404ef1dd0d16224a826eed41c0e90&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22wanted_FE_ad-list-data-set.json%22&x-id=GetObject`;
-  const [advs, setAdvs] = useState(null);
-  useEffect(() => {
-    getData(baseURL).then((data) => setAdvs(data));
-  }, []);
+  const getAd = useRecoilValue(getAdvertisState);
+  const handlerGetData = async () => {
+    const res = await getAd();
+    /* eslint-disable no-console */
+    console.log(res.ads);
+    /* eslint-disable no-console */
+  };
 
   return (
-    <div>
-      {advs.map((advs) => (
-        <li key={advs}>{advs}</li>
-      ))}
-    </div>
+    <>
+      <div onClick={handlerGetData}>전체광고</div>
+    </>
   );
 };
 
