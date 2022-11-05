36d5fbd8ecf58bb1eef1b703a07073530b2cb939
jungmin <qkrwjdals302@naver.com>
2022-11-06 06:45:39 +0900

    Feat: 전역상태, 디폴트템플릿, 라우팅 처리 반영

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import DefaultTemplate from "template/DefaultTemplate";
+import AdManagement from "pages/AdManagement";
+import Dashboard from "pages/Dashboard";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <RecoilRoot>
+      <Router>
+        <DefaultTemplate>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/admanagement" element={<AdManagement />} />
+          </Routes>
+        </DefaultTemplate>
+      </Router>
+    </RecoilRoot>
+  );
 }
 
 export default App;
