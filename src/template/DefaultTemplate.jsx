2554d3f8c0782be95edb19b572ad20dd81b5eea4
jungmin <qkrwjdals302@naver.com>
2022-11-06 06:43:36 +0900

    Feat: DefaultTemplate 구현 - 모든 페이지에서 공통적으로 사용되는 Lnb, Header, HeaderNav를 디폴트템플릿으로 구현

---
+++ src/template/DefaultTemplate.jsx
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+import HeaderNav from "components/HeaderNav";
+import Lnb from "components/Lnb";
+
+const DefaultTemplate = ({ children }) => {
+  return (
+    <Container>
+      <Lnb />
+      <PageWrapper>
+        <HeaderNav />
+        {/* // TODO: HEADER 추가해야함 */}
+        {children}
+      </PageWrapper>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default DefaultTemplate;
