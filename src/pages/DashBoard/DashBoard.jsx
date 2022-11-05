333bb32e703b11aa9e5704170c00f1f652e1178d
YeonghunKO <yhko1988@gmail.com>
2022-11-05 23:56:02 +0900

    feat : get 함수 전역화

--- src/pages/DashBoard/DashBoard.jsx
+++ src/pages/DashBoard/DashBoard.jsx
@@ -1,5 +1,13 @@
+import { useRecoilValue } from "recoil";
+import { getAdvertismentState } from "atoms/advertisment";
+
 function DashBoard() {
-  return <div>dashboard</div>;
+  const getAdvertisment = useRecoilValue(getAdvertismentState);
+  const handleGetAdvertisment = async () => {
+    const result = await getAdvertisment("/trend_data/1");
+    console.log(result);
+  };
+  return <div onClick={handleGetAdvertisment}>dashboard</div>;
 }
 
 export default DashBoard;
