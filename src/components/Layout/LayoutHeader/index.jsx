9546aeae8ca4eba78e4058e9d6a7120f1e3ba68b
Sangmin <23mean@naver.com>
2022-11-07 02:35:31 +0900

    feat: layout(header, sidenav) 구조 추가

---
+++ src/components/Layout/LayoutHeader/index.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+import Icon from "components/common/Icon";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  width: 100%;
+  height: 80px;
+
+  gap: 24px;
+`;
+
+const IconWrapper = styled.div`
+  cursor: pointer;
+`;
+
+const UserInfoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  gap: 10px;
+
+  margin-right: 40px;
+
+  font-family: Roboto;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 16px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #3a474e;
+
+  cursor: pointer;
+`;
+
+const LayoutHeader = () => {
+  return (
+    <Wrapper>
+      <IconWrapper>
+        <Icon icon="Alarm" />
+      </IconWrapper>
+      <IconWrapper>
+        <Icon icon="Setting" />
+      </IconWrapper>
+
+      <UserInfoWrapper>
+        <Icon icon="DefaultUser" size={40} />
+        원티드님
+      </UserInfoWrapper>
+    </Wrapper>
+  );
+};
+
+export default LayoutHeader;
---
+++ src/components/Layout/LayoutNav/index.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+
+import NavService from "./NavService";
+import NavAdCenter from "./NavAdCenter";
+import NavGuide from "./NavGuide";
+import NavFooter from "./NavFooter";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 320px;
+  height: 100%;
+`;
+
+const NavLogo = styled.img`
+  width: 124px;
+
+  margin: 40px 60px 100px 0;
+
+  cursor: pointer;
+`;
+
+const LayoutNav = () => {
+  return (
+    <Wrapper>
+      <Link to="/">
+        <NavLogo src="/LEVER_LOGO.png" alt="COMPANY_LOGO" />
+      </Link>
+
+      <NavService />
+      <NavAdCenter />
+      <NavGuide />
+      <NavFooter />
+    </Wrapper>
+  );
+};
+
+export default LayoutNav;
