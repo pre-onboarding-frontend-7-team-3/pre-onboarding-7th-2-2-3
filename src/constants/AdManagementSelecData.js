e0855b8fcc84cb68d7e1916fed6a2c95a5331bb2
YeonghunKO <yhko1988@gmail.com>
2022-11-06 20:50:39 +0900

    refactor : 파일 위치 변경

--- src/constants/AdManagementSelecData.js
+++ src/constants/AdManagementSelecData.js
@@ -1,7 +1,15 @@
-export const AD_MANAGEMENT_DATA = [
+const AD_MANAGEMENT_DATA = [
   { name: "전체 광고" },
   { name: "진행중인 광고" },
   { name: "종료된 광고" },
 ];
 
-export const CREATE_AD = "광고 만들기";
+const CARD_STATUS = {
+  ALL: "all",
+  ENDED: "ended",
+  ACTIVE: "active",
+};
+
+const CREATE_AD = "광고 만들기";
+
+export { AD_MANAGEMENT_DATA, CARD_STATUS, CREATE_AD };
---
+++ src/store/selector/trendDataQueryState.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { selector } from "recoil";
+
+export const trendDataQuery = selector({
+  key: "trendDataQuery",
+  get: async ({ get }) => {
+    // const date = get(dateState);
+    const res = await axios.get("http://localhost:4000/trend_data");
+    return res.data;
+  },
+});
