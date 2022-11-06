ce692939135d390d4b2e7456e4f57bcf73305427
PiperChang <goddhqj8000@gmail.com>
2022-11-06 18:06:07 +0900

    style: infoBoxes 스타일

--- src/components/Home/ComparePerformance/InfoBoxes.jsx
+++ src/components/Home/ComparePerformance/InfoBoxes.jsx
@@ -1,104 +1,134 @@
-import styled from '@emotion/styled';
-import React from 'react'
+import styled from "@emotion/styled";
+import React from "react";
 import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
-import unitDecider from 'utils/unitDecider';
+import unitDecider from "utils/unitDecider";
 
-export const ROAS = ({value, exValue}) => {
-  const diff = value - exValue 
+export const ROAS = ({ value, exValue }) => {
+  const diff = value - exValue;
   return (
-    <div>
-      <h2>ROAS</h2>
-      <p>{parseInt(value)}%</p>
-      <div>
-        {diff > 0? <AiOutlineCaretUp/> : <AiOutlineCaretDown/> }
-        {parseInt(Math.abs(diff))}%
-      </div>
-    </div>
-  )
-}
-export const Cost = ({value, exValue}) => {
-  const diff = value - exValue
-  return (
-    <div>
-      <h2>광고비</h2>
-      <p>{unitDecider(parseInt(value))} 원</p>
-      <div>
-        {diff > 0? <AiOutlineCaretUp/> : <AiOutlineCaretDown/> }
-        {unitDecider(Math.abs(parseInt(diff)))} 원
-      </div>
-    </div>
-  )
-}
-
-export const Imp = ({value, exValue}) => {
-  const diff = value - exValue
-  return (
-    <div>
-      <h2>노출수</h2>
-      <p>{unitDecider(parseInt(value))} 회</p>
-      <div>
-        {diff > 0? <AiOutlineCaretUp/> : <AiOutlineCaretDown/> }
-        {unitDecider(Math.abs(parseInt(diff)))} 회
-      </div>
-    </div>
-  )
-}
-
-export const Click = ({value, exValue}) => {
-  const diff = value - exValue
-  return (
-    <div>
-      <h2>클릭수</h2>
-      <p>{unitDecider(value)} 회</p>
-      <div>
-        {diff > 0? <AiOutlineCaretUp/> : <AiOutlineCaretDown/> }
-        {unitDecider(Math.abs(diff))} 회
-      </div>
-    </div>
-  )
-}
-export const Conv = ({value, exValue}) => {
-  const diff = value - exValue
-  return (
-    <div>
-      <h2>전환수</h2>
-      <p>{parseInt(value)} 회</p>
-      <div>
-        {diff > 0? <AiOutlineCaretUp/> : <AiOutlineCaretDown/> }
-        {Math.abs(diff)} 회
-      </div>
-    </div>
-  )
-}
-export const Revenue = ({value, exValue}) => {
-  const diff = value - exValue
-  return (
-    // <InfoBoxWrapper>
     <InfoBoxWrapper>
-      <TitleBox >
-        <h2>매출</h2>
+    <TitleBox>
+      <h3>ROAS</h3>
+      <p>{parseInt(value)}%</p>
+      </TitleBox>
+      <RightBox>
+        {diff > 0 ? <AiOutlineCaretUp color="green" /> : <AiOutlineCaretDown color="red" />}
+        {parseInt(Math.abs(diff))}%
+        </RightBox>
+    </InfoBoxWrapper>
+  );
+};
+export const Cost = ({ value, exValue }) => {
+  const diff = value - exValue;
+  return (
+    <InfoBoxWrapper>
+      <TitleBox>
+      <h3>광고비</h3>
+      <p>{unitDecider(parseInt(value))} 원</p>
+      </TitleBox>
+      <RightBox>
+        {diff > 0 ? <AiOutlineCaretUp color="green" /> : <AiOutlineCaretDown color="red" />}
+        {unitDecider(Math.abs(parseInt(diff)))} 원
+        </RightBox>
+    </InfoBoxWrapper>
+  );
+};
+
+export const Imp = ({ value, exValue }) => {
+  const diff = value - exValue;
+  return (
+    <InfoBoxWrapper>
+    <TitleBox>
+      <h3>노출수</h3>
+      <p>{unitDecider(parseInt(value))} 회</p>
+      </TitleBox>
+      <RightBox>
+        {diff > 0 ? <AiOutlineCaretUp color="green" /> : <AiOutlineCaretDown color="red" />}
+        {unitDecider(Math.abs(parseInt(diff)))} 회
+        </RightBox>
+    </InfoBoxWrapper>
+  );
+};
+
+export const Click = ({ value, exValue }) => {
+  const diff = value - exValue;
+  return (
+    <InfoBoxWrapper>
+      <TitleBox>
+      <h3>클릭수</h3>
+      <p>{unitDecider(value)} 회</p>
+      </TitleBox>
+      <RightBox>
+        {diff > 0 ? <AiOutlineCaretUp color="green" /> : <AiOutlineCaretDown color="red" />}
+        {unitDecider(Math.abs(diff))} 회
+        </RightBox>
+    </InfoBoxWrapper>
+  );
+};
+export const Conv = ({ value, exValue }) => {
+  const diff = value - exValue;
+  return (
+    <InfoBoxWrapper>
+      <TitleBox>
+        <h3>전환수</h3>
+        <p>{parseInt(value)} 회</p>
+      </TitleBox>
+      <RightBox>
+        {diff > 0 ? <AiOutlineCaretUp color="green" /> : <AiOutlineCaretDown color="red" />}
+        {Math.abs(diff)} 회
+      </RightBox>
+    </InfoBoxWrapper>
+  );
+};
+export const Revenue = ({ value, exValue }) => {
+  const diff = value - exValue;
+  return (
+    <InfoBoxWrapper>
+      <TitleBox>
+        <h3>매출</h3>
         <p>{unitDecider(parseInt(value))} 원</p>
       </TitleBox>
-      <div>
-        {diff > 0? <AiOutlineCaretUp/> : <AiOutlineCaretDown/> }
+      <RightBox>
+        {diff > 0 ? <AiOutlineCaretUp color="green" /> : <AiOutlineCaretDown color="red" />}
         {unitDecider(Math.abs(diff))} 원
-      </div>
+      </RightBox>
     </InfoBoxWrapper>
-  )
-}
+  );
+};
 
 const InfoBoxWrapper = styled.div`
-    border :1px solid red;
-    margin:10px;
-    width: 
+  border: 0.5px solid #EDEFF1;
+  border-radius: 10px;;
+  margin: 10px;
+  padding: 18px 40px;
+  position: relative;
   div {
-    float:left;
-    /* border :1px solid red; */
-    /* width:50%; */
-    
-
+    float: left;
   }
-`
+`;
 const TitleBox = styled.div`
-  width:80%;
-`
+  
+  h3 {
+    color: #94a2ad;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+  }
+  p {
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    color: #3a474e;
+  }
+`;
+const RightBox = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 40px 40px;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 14px;
+  text-align: right;
+  color: #3a474e;
+`;
