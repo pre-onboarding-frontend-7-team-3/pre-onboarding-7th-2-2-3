7aa8a0d93a42a664cb64bdc22422c44e9223cdde
YeonghunKO <yhko1988@gmail.com>
2022-11-06 09:07:16 +0900

    feat : dropdown 메뉴 생성!

---
+++ src/components/dropDown/DropDown.jsx
@@ -0,0 +1,29 @@
+import { FormControl, MenuItem, Select } from "@mui/material";
+import { useState } from "react";
+
+function DropDown({ menuItems, prefixIcon = "", standard, style = { m: 1, minWidth: 120 } }) {
+  const [option, setOption] = useState(menuItems[0].text);
+
+  const handleChange = (event) => {
+    setOption(event.target.value);
+  };
+  return (
+    <FormControl variant={standard && "standard"} sx={style}>
+      <Select
+        autoWidth
+        value={option}
+        onChange={handleChange}
+        inputProps={{ "aria-label": "Without label" }}
+        renderValue={(text) => `${prefixIcon} ${text}`}
+      >
+        {menuItems.map((menuItem, idx) => (
+          <MenuItem key={idx} value={menuItem.text}>
+            {menuItem.text}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
+
+export default DropDown;
