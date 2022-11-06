dbbcf75c82b7cdd6372516951297be132c010365
YeonghunKO <yhko1988@gmail.com>
2022-11-06 09:05:42 +0900

    style : global style 및 reset css 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import DashBoard from "pages/DashBoard";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { GlobalStyle, Theme } from "shared/global.style";
+import { ThemeProvider } from "styled-components";
 
 function App() {
   return (
     <RecoilRoot>
-      <Routes>
-        <Route path="/" element={<DashBoard />} />
-      </Routes>
+      <GlobalStyle />
+      <ThemeProvider theme={Theme}>
+        <React.Suspense fallback={<div>..loading</div>}>
+          <Routes>
+            <Route path="/" element={<DashBoard />} />
+          </Routes>
+        </React.Suspense>
+      </ThemeProvider>
     </RecoilRoot>
   );
 }
