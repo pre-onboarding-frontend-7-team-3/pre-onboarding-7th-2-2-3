11675173ec7a329f8d009649a40a1b33b45e7649
Sangmin <23mean@naver.com>
2022-11-07 02:34:12 +0900

    feat: nav menu 반응형 구현 hamberger icon 추가

--- src/components/common/icons/index.js
+++ src/components/common/icons/index.js
@@ -2,6 +2,7 @@ export { default as DashBoard } from "./DashBoard";
 export { default as ManageAds } from "./ManageAds";
 export { default as Guide } from "./Guide";
 
+export { default as SideNavToggle } from "./SideNavToggle";
 export { default as Alarm } from "./Alarm";
 export { default as Setting } from "./Setting";
 export { default as DefaultUser } from "./DefaultUser";
