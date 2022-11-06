8cf41955c513edce39d991bf699f1038d133b784
moonkorea00 <moonkorea00@gmail.com>
2022-11-06 22:56:17 +0900

    feat : adList 데이터, 필터링 상태 및 필터 데이터 store에서 관리

--- src/store/atoms/cards.js
+++ src/store/atoms/cards.js
@@ -1,6 +1,37 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+import axios from "axios";
 
-export const cardsState = atom({
-  key: "cardState",
-  default: [],
+export const adListFilterState = atom({
+  key: "adListFilter",
+  default: "전체 광고",
+});
+
+export const adListState = selector({
+  key: "adListState",
+  get: async ({ get }) => {
+    const { data } = await axios.get("/http://localhost:4000/ads");
+    return data.ads;
+  },
+});
+
+export const adListFilterQuery = selector({
+  key: "adListFilterQuery",
+  get: ({ get }) => {
+    const filter = get(adListFilterState);
+    const adList = get(adListState);
+
+    switch (filter) {
+      case "전체 광고": {
+        return adList;
+      }
+      case "진행중인 광고": {
+        return adList.filter((ad) => ad.status === "active");
+      }
+      case "종료된 광고": {
+        return adList.filter((ad) => ad.status === "ended");
+      }
+      default:
+        throw new Error("Error finding adList");
+    }
+  },
 });
