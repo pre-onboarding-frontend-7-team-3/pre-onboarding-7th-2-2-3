6b01b7ff3b04c015c950fa018b7a9b87e7838d69
PiperChang <goddhqj8000@gmail.com>
2022-11-06 12:28:22 +0900

    chore: json-server setting

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import CompareGraph from "components/Home/CompareGraph/CompareGraph";
 import ComparePerformance from "components/Home/ComparePerformance/ComparePerformance";
-import HeaderNav from "components/Home/HeaderNav/HeaderNav";
-import React from "react";
+// import HeaderNav from "components/Home/HeaderNav/HeaderNav";
+import React, { Suspense } from "react";
 
 const Home = () => {
   return (
     <>
-      <HeaderNav />
-      <ComparePerformance />
+      {/* <HeaderNav /> */}
+      <Suspense fallback={<div>로딩중...</div>}>
+        <ComparePerformance />
+      </Suspense>
       <CompareGraph />
     </>
   );
