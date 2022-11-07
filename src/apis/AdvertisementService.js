113606449df886263c1f38f86606a06c423a4144
YeonghunKO <yhko1988@gmail.com>
2022-11-07 10:39:45 +0900

    refactor : api 관련 함수를 oop로 만들었음
    관심이 확실히 분리되어있어서 재사용성이 증가한다.

---
+++ src/apis/AdvertisementService.js
@@ -0,0 +1,26 @@
+export class AdvertisementService {
+  #httpClient;
+
+  constructor(httpClient) {
+    this.#httpClient = httpClient;
+  }
+
+  get client() {
+    return this.#httpClient;
+  }
+
+  async get(url = "") {
+    const { data } = await this.client.request({ method: "get", url });
+
+    return data;
+  }
+
+  async edit({ id, ...rest }) {
+    const { data } = await this.client.request({
+      method: "patch",
+      url: `/${id}`,
+      data: { ...rest },
+    });
+    return data;
+  }
+}
